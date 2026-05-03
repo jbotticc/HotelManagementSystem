a343dfb1a363248fb08a8eb9d43a7c671d4e70c4
Tanner123g <tanner123gleason@gmail.com>
2026-05-03 14:10:18 -0400

    immutable guest

--- Domain/Models/Guest.cs
+++ Domain/Models/Guest.cs
@@ -2,15 +2,14 @@ namespace HotelManagementSystem.Domain.Models;
 
 public struct Guest
 {
-    private string _name;
-    private string _phoneNumber;
+    private readonly string _name;
+    private readonly string _phoneNumber;
 
-
-    private DateTime _checkoutDate;
+    private readonly DateTime _checkoutDate;
 
     public Guest(string name, string phoneNumber, DateTime checkoutDate)
     {
-      if (string.IsNullOrWhiteSpace(name))
+        if (string.IsNullOrWhiteSpace(name))
             throw new ArgumentException("Guest name cannot be null or empty.");
 
         if (string.IsNullOrWhiteSpace(phoneNumber))
@@ -24,31 +23,16 @@ public struct Guest
     public string Name
     {
         get => _name;
-        set 
-        {
-            if (string.IsNullOrWhiteSpace(value))
-                throw new ArgumentException("Guest name cannot be null or empty.");
-
-            _name = value;
-        }
     }
 
     public string PhoneNumber
     {
         get => _phoneNumber;
-        set 
-         {
-            if (string.IsNullOrWhiteSpace(value))
-                throw new ArgumentException("Phone number cannot be null or empty.");
-
-            _phoneNumber = value;
-        }
     }
 
     public DateTime CheckoutDate
     {
         get => _checkoutDate;
-        set => _checkoutDate = value;
     }
 
 
